4cdd023d8043ed1e204a8cb2758ea9e8e63538d1
ZashQash <ZashQash@users.noreply.github.com>
2022-08-09 07:42:27 +0000

    ✅ Ready to clone and code.

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-"""Python setup.py for project_name package"""
+"""Python setup.py for datacard_analyzer package"""
 import io
 import os
 from setuptools import find_packages, setup
@@ -6,7 +6,7 @@ from setuptools import find_packages, setup
 
 def read(*paths, **kwargs):
     """Read the contents of a text file safely.
-    >>> read("project_name", "VERSION")
+    >>> read("datacard_analyzer", "VERSION")
     '0.1.0'
     >>> read("README.md")
     ...
@@ -30,17 +30,17 @@ def read_requirements(path):
 
 
 setup(
-    name="project_name",
-    version=read("project_name", "VERSION"),
-    description="project_description",
-    url="https://github.com/author_name/project_urlname/",
+    name="datacard_analyzer",
+    version=read("datacard_analyzer", "VERSION"),
+    description="Awesome datacard_analyzer created by ZashQash",
+    url="https://github.com/ZashQash/datacard_analyzer/",
     long_description=read("README.md"),
     long_description_content_type="text/markdown",
-    author="author_name",
+    author="ZashQash",
     packages=find_packages(exclude=["tests", ".github"]),
     install_requires=read_requirements("requirements.txt"),
     entry_points={
-        "console_scripts": ["project_name = project_name.__main__:main"]
+        "console_scripts": ["datacard_analyzer = datacard_analyzer.__main__:main"]
     },
     extras_require={"test": read_requirements("requirements-test.txt")},
 )
